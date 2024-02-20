d499bed744b7e3f355945d2527005ba536293fbd
Shahzod <shahzodwill@gmail.com>
2024-02-20 01:33:24 +0100

    display cart/cart total price

--- scripts/displayCart.js
+++ scripts/displayCart.js
@@ -1,7 +1,14 @@
-function generateCartItem (filmItem) {
-    const cartItemsContainer = document.querySelector(".cart-items")
+let filmsArray = JSON.parse(localStorage.getItem("cart"));
+const priceSpan = document.querySelector('.cart-total-price');
+console.log(priceSpan)
 
+
+
+
+function generateCartItem (filmItem) {
+    const cartItemsContainer = document.querySelector(".cart-items");
     const cartRow = document.createElement("div");
+    cartRow.setAttribute('id', filmItem.id)
     cartRow.classList.add("cart-row");
 
     const cartItem = document.createElement("div");
@@ -34,11 +41,12 @@ function generateCartItem (filmItem) {
     cartItem.appendChild(removeButton);
     cartRow.appendChild(cartItem);
     cartItemsContainer.appendChild(cartRow);
+
+    priceSpan.textContent = updateCartTotal();
 };
 
 
 function getFilms() {
-    const filmsArray = JSON.parse(localStorage.getItem("cart"));
     filmsArray.forEach(film => {
         generateCartItem(film)
     })  
@@ -46,8 +54,34 @@ function getFilms() {
 
 function removeCartItem(event) {
     const removeButtonClicked = event.target;
-        removeButtonClicked.parentElement.parentElement.remove()
-        updateCartTotal();
+    const toRemove = removeButtonClicked.closest('.cart-row').id
+    removeButtonClicked.closest('.cart-row').remove()
+    const indexToRemove = filmsArray.findIndex(item => item.id === toRemove)
+    if(indexToRemove !== -1){
+        filmsArray.splice(indexToRemove, 1)
+        localStorage.setItem("cart", JSON.stringify(filmsArray))
+        priceSpan.textContent = updateCartTotal();
+    }
+    
+}
+// function updateCartTotal() {
+//     const cartItemContainer = document.getElementsByClassName("cart-items")[0];
+//     const cartRows = cartItemContainer.getElementsByClassName("cart-row");
+//     let total = 0;
+//     for (var i = 0; i < cartRows.length; i++) {
+//         const cartRow = cartRows[i];
+//         const priceElement = cartRow.getElementsByClassName("cart-price")[0]
+//         const quantityElement = cartRow.getElementsByClassName("cart-quantity-input")[0];
+//         const price = parseFloat(priceElement.innerText.replace("kr", ""));
+//         const quantity = quantityElement.value;
+//         total = total + (price * quantity);
+//     }
+//     total = Math.round(total * 100) / 100;          // to desimaler etter komma
+//     document.getElementsByClassName("cart-total-price")[0].innerText = total + ' kr';
+// }
+function updateCartTotal() {
+    const totalPrice = filmsArray.reduce((total, item) => total + item.price, 0)
+    return totalPrice
 }
 
 getFilms();
