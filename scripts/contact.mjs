1d593ac9d771917a3e8d49da5bfbd0cde7f13ccf
EliNygard <eli.nygard@gmail.com>
2024-02-25 14:22:39 +0100

    clear content in form after clicking

--- scripts/contact.mjs
+++ scripts/contact.mjs
@@ -3,6 +3,10 @@ const submitButton = document.querySelector('#submit');
 export async function onSubmitClick (event) {
     event.preventDefault();
     alert("The message was sent");
+
+    document.querySelector("#name").value = "";
+    document.querySelector("#email").value = "";
+    document.querySelector("#message").value = "";
 };
 
 
