56c2e11d7318c16702e0069eb6d6f45f7c0dfbbc
EliNygard <eli.nygard@gmail.com>
2024-10-11 18:02:33 +0200

    adding fixture file and start writing test

---
+++ cypress/e2e/films.cy.js
@@ -0,0 +1,11 @@
+// test if user can add item to the cart
+// 
+
+describe("Films", () => {
+    // beforeEach?
+    // afterEach?
+
+    // it should add film to cart
+
+    // it should remove film from cart
+})
